c5f75f2cf3f226547c0c36b3c6d2f51ec1672b17
Roman Joost <rjoost@redhat.com>
2017-02-20 19:24:05 +1000

    Parse ByteStrings instead of Text
    Aeson parses Bytestrings and thinking about it, can't really think of a
    reason why we would want to use Text here.

--- src/Parser.hs
+++ src/Parser.hs
@@ -7,47 +7,43 @@ import Data.Conduit.Attoparsec (conduitParser)
 import Control.Applicative ((<|>))
 import Data.Conduit (($$), ($=))
 import qualified Data.Conduit.Combinators as C
-import Data.Conduit.Text (decode, utf8)
-import Text.PrettyPrint.Leijen (text, (<>), Pretty(..), fillCat, cat, softbreak)
+import Text.PrettyPrint.Leijen (text, (<>), Pretty(..), fillCat, cat, softbreak, Doc)
 import Data.Char (isControl)
-import qualified Data.Text as T
+import qualified Data.ByteString.Char8 as B
 import Prelude hiding (take)
-import Data.Attoparsec.Text.Lazy
+import Data.Attoparsec.ByteString.Char8
        (Parser, string, char, many1, skipSpace,
         takeTill, manyTill, anyChar, many', option)
 
-instance Pretty T.Text where
-  pretty t = text $ T.unpack t
-
 instance Pretty Play where
-  pretty (Play h xs) = (text $ T.unpack h) <> softbreak <> (fillCat $ (\t -> softbreak <> pretty t) <$> xs)
+  pretty (Play h xs) = (text $ B.unpack h) <> softbreak <> (fillCat $ (\t -> softbreak <> pretty t) <$> xs)
 
 instance Pretty Task where
-  pretty (Task n xs) = (text $ T.unpack n) <> softbreak <> (cat $ (\t -> pretty t) <$> xs)
+  pretty (Task n xs) = (text $ B.unpack n) <> softbreak <> (cat $ (\t -> pretty t) <$> xs)
 
 instance Pretty TaskOutput where
-  pretty (TaskOutput st h yaml) = pretty st <> pretty h <> (text $ T.unpack yaml)
+  pretty (TaskOutput st h yaml) = pretty st <> pretty (B.unpack h) <> (text $ B.unpack yaml)
 
 instance Pretty TaskState where
   pretty OK = text "ok"
   pretty Skipping = text "skipping"
   pretty _ = text "whatever"
 
-data Play = Play T.Text [Task]
+data Play = Play B.ByteString [Task]
   deriving (Show)
 
 data Task =
-  Task {name :: T.Text
+  Task {name :: B.ByteString
        ,taskoutput :: [TaskOutput]}
   deriving (Show, Eq)
 
 data TaskOutput =
     TaskOutput TaskState
-               T.Text
+               B.ByteString
                SomeYAML
     deriving (Show,Eq)
 
-type SomeYAML = T.Text
+type SomeYAML = B.ByteString
 
 data TaskState
   = OK
@@ -67,12 +63,12 @@ parsePlay = do
 parseTask :: Parser Task
 parseTask = do
   skipSpace
-  name <- takeTill isControl
-  char '\n'
+  n <- takeTill isControl
+  _ <- char '\n'
   timestamp <- takeTill isControl
   verbose <- many' $ string "<" *> takeTill isControl
   output <- many' parseTaskOutput
-  pure $ Task { name = T.concat [name, "\n", timestamp, T.concat verbose], taskoutput = output}
+  pure $ Task { name = B.concat [n, "\n", timestamp, B.concat verbose], taskoutput = output}
 
 parseTaskOutput :: Parser TaskOutput
 parseTaskOutput = do
@@ -80,8 +76,8 @@ parseTaskOutput = do
   h <- string " [" *> manyTill anyChar (string "]")
   y <- parseYAML
   case y of
-    Just yaml -> pure $ TaskOutput s (T.pack h) yaml
-    Nothing -> pure $ TaskOutput s (T.pack h) ""
+    Just yaml -> pure $ TaskOutput s (B.pack h) yaml
+    Nothing -> pure $ TaskOutput s (B.pack h) ""
 
 parseYAML :: Parser (Maybe SomeYAML)
 parseYAML = option Nothing (Just <$> (string " => " *> takeTill isControl))
@@ -96,4 +92,4 @@ parseTaskState =
 conduitPretty = C.map (\(_, p) -> pretty p)
 
 main :: IO ()
-main = runResourceT $ sourceHandle stdin $= decode utf8 $= conduitParser parsePlay $$ conduitPretty $= C.print
+main = runResourceT $ sourceHandle stdin $= conduitParser parsePlay $$ conduitPretty $= C.print
